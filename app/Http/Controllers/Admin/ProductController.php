001f06f64bdc057b5384a43dea33fa2dd002fd58
Mohamad Elhadidy <elhadidy384@gmail.com>
2025-02-03 13:20:09 +0200

    revert pas commit

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -73,12 +73,6 @@ class ProductController extends Controller
 
 
             if ($request->has('images')) {
-
-                if (! File::exists(storage_path('app/public/products'))) {
-                    File::makeDirectory(storage_path('app/public/products'), 0777, true);
-                }
-
-
                 try {
                     // Process images
                     foreach ($request->input('images') as $base64Image) {
